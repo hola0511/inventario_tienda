cf31c5b7dbc45437284a6f152985488f2a635812
hola0511 <espinosaariassamuel@gmail.com>
2024-11-14 23:54:41 -0500

    cambios para cumplir aceptacion

--- app.py
+++ app.py
@@ -96,5 +96,7 @@ def producto(index):
     return render_template('producto.html', producto=producto)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
